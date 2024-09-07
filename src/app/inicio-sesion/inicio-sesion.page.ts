f0e9fdcf8f03c2b1d223887d6a58a12ede9d83e0
Hugo Romero <hu.romerog@duocuc.cl>
2024-09-06 20:19:48 -0400

    l

--- src/app/inicio-sesion/inicio-sesion.page.ts
+++ src/app/inicio-sesion/inicio-sesion.page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-
+import { AnimationController } from '@ionic/angular';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.page.html',
   styleUrls: ['./inicio-sesion.page.scss'],
 })
 export class InicioSesionPage implements OnInit {
+  icono = 'oscuro';
   usuario: string = '';
   clave: string = '';
   sw: boolean = false;
@@ -16,25 +18,55 @@ export class InicioSesionPage implements OnInit {
 
   // Función que se ejecuta al presionar el botón "Iniciar sesión"
   login() {
-    // Mostrar la alerta con un mensaje y una acción vacía
-    this.alerta(`Usuario: ${this.usuario} - Contraseña: ${this.clave}`, () => {
-      console.log('Alerta aceptada');
-    });
+    // Validar si el campo de usuario tiene una "@" y ambos campos están llenos
+    if (
+      this.usuario.includes('@') &&
+      this.usuario.trim() !== '' &&
+      this.clave.trim() !== ''
+    ) {
+      if (this.clave.length !== 8) {
+        this.alerta('La contraseña debe tener minimo 8 caracteres', () => {
+          console.log('Error en la validación');
+        });
+      } else {
+        this.router.navigate(['bilbao']); // Cambia '/pagina-destino' por la ruta de destino
+      }
+    } else {
+      // Mostrar alerta si no se cumplen las condiciones
+      this.alerta(
+        'Por favor, ingresa un correo válido y completa todos los campos',
+        () => {
+          console.log('Error en la validación');
+        }
+      );
+    }
   }
 
   // Función que muestra una alerta
   alerta(texto: string, accion: () => void) {
-    this.alert.create({
-      header: 'Información',
-      message: texto,
-      buttons: [
-        {
-          text: 'Aceptar',
-          handler: accion,
-        },
-      ],
-    }).then(alert => alert.present());  // Mostrar la alerta
+    this.alert
+      .create({
+        header: 'Error',
+        message: texto,
+        buttons: [
+          {
+            text: 'Aceptar',
+            handler: accion,
+          },
+        ],
+      })
+      .then((alert) => alert.present()); // Mostrar la alerta
   }
-
+  cambiarTema() {
+    if (this.icono == 'oscuro') {
+      document.documentElement.style.setProperty('--fondo', '#fff1122');
+      document.documentElement.style.setProperty(' --fondo-input', '#1f3d59');
+      this.icono = 'claro';
+    } else {
+      document.documentElement.style.setProperty('--fondo', '#d7d7d7');
+      document.documentElement.style.setProperty(' --fondo-input', '#1f3d59');
+      this.icono = 'oscuro';
+    }
+  }
   ngOnInit() {}
 }
