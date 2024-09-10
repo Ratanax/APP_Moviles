b950e5c953831225d9bde1d401e712e70e81a553
Lucaaas <luc.padilla@duocuc.cl>
2024-09-10 00:16:58 -0300

    cambiar color

--- src/app/asistencia/asistencia.page.ts
+++ src/app/asistencia/asistencia.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asistencia.page.scss'],
 })
 export class AsistenciaPage implements OnInit {
-
+  icono = 'oscuro';
   items: string[];
   showInfo: boolean[];
 
@@ -27,5 +27,27 @@ export class AsistenciaPage implements OnInit {
     
     this.showInfo[index] = !this.showInfo[index];
   }
+
+  cambiarTema() {
+    if (this.icono == 'oscuro') {
+      document.documentElement.style.setProperty('--fondo', '#2e2d2d');
+      document.documentElement.style.setProperty('--fondo-input', '#2e2d2d');
+      document.documentElement.style.setProperty('--icono-tema', '#f0cc00');
+      document.documentElement.style.setProperty('--seccion', '#1f1f1f');
+      document.documentElement.style.setProperty('--texto-input', 'white');
+      document.documentElement.style.setProperty('--ion-color-success', 'white');
+
+      this.icono = 'claro';
+    } else {
+      document.documentElement.style.setProperty('--fondo', '#e8e6e6');
+      document.documentElement.style.setProperty('--fondo-input', '#e8e6e6');
+      document.documentElement.style.setProperty('--icono-tema', '#8c8c8c');
+      document.documentElement.style.setProperty('--seccion', '#cfcfcf');
+      document.documentElement.style.setProperty('--texto-input', 'black');
+      document.documentElement.style.setProperty('--ion-color-success', 'black');
+      this.icono = 'oscuro';
+    }
+  }
+
 }
 
