6b66d96b62757819056012e5dbb35283d27205c1
Lucaaas <luc.padilla@duocuc.cl>
2024-10-20 14:53:21 -0300

    icono de cambiar tema movido dentro del contenedor, foto chafa del qr remplazada por el scanner.

--- src/app/asistencia/asistencia.page.ts
+++ src/app/asistencia/asistencia.page.ts
@@ -48,19 +48,20 @@ export class AsistenciaPage implements OnInit {
   }
 
   cambiarTema() {
-    document.documentElement.style.setProperty('--fondo',this.icono == 'oscuro' ? '#2e2d2d' : '#e8e6e6');
-    document.documentElement.style.setProperty('--fondo-input',this.icono == 'oscuro' ? '#2e2d2d' : '#e8e6e6');
-    document.documentElement.style.setProperty('--icono-tema',this.icono == 'oscuro' ? '#f0cc00' : '#8c8c8c');
-    document.documentElement.style.setProperty('--seccion',this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
-    document.documentElement.style.setProperty('--texto-input',this.icono == 'oscuro' ? 'white' : 'black');
-    document.documentElement.style.setProperty('--ion-color-success',this.icono == 'oscuro' ? 'white' : 'black');
-    document.documentElement.style.setProperty('--fondo-borde',this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
-    document.documentElement.style.setProperty('--color-list',this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
-    document.documentElement.style.setProperty('--item-bg',this.icono == 'oscuro' ? '#1f1f1f' : 'white');
-    
+    document.documentElement.style.setProperty('--fondo', this.icono == 'oscuro' ? '#2e2d2d' : '#e8e6e6');
+    document.documentElement.style.setProperty('--fondo-input', this.icono == 'oscuro' ? '#2e2d2d' : '#e8e6e6');
+    document.documentElement.style.setProperty('--icono-tema', this.icono == 'oscuro' ? '#f0cc00' : '#8c8c8c');
+    document.documentElement.style.setProperty('--seccion', this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
+    document.documentElement.style.setProperty('--texto-input', this.icono == 'oscuro' ? 'white' : 'black');
+    document.documentElement.style.setProperty('--ion-color-success', this.icono == 'oscuro' ? 'white' : 'black');
+    document.documentElement.style.setProperty('--fondo-borde', this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
+    document.documentElement.style.setProperty('--color-list', this.icono == 'oscuro' ? '#1f1f1f' : '#cfcfcf');
+    document.documentElement.style.setProperty('--item-bg', this.icono == 'oscuro' ? '#1f1f1f' : 'white');
+    document.documentElement.style.setProperty('--item-text-color', this.icono == 'oscuro' ? 'white' : 'black'); // Color del texto en la lista
+  
     this.icono = this.icono == 'oscuro' ? 'claro' : 'oscuro';
     localStorage.setItem('tema', this.icono);
     console.log(this.icono);
-    
   }
+  
 }
